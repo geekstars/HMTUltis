1ef4432828beb552190dead53bea54dbee539d8a
Hoàng Minh Thành <hoangminhthanh@outlook.com>
2020-01-02 17:06:03 +0700

    Initial Commit

---
+++ HMTUltis/HMTUltis.h
@@ -0,0 +1,19 @@
+//
+//  HMTUltis.h
+//  HMTUltis
+//
+//  Created by Hoàng Minh Thành on 1/2/20.
+//  Copyright © 2020 Hoàng Minh Thành. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HMTUltis.
+FOUNDATION_EXPORT double HMTUltisVersionNumber;
+
+//! Project version string for HMTUltis.
+FOUNDATION_EXPORT const unsigned char HMTUltisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMTUltis/PublicHeader.h>
+
+
